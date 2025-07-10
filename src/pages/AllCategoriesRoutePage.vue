73e9da9e777817b93a8fc5091452751c4f4c436d
Maxim <m.sergeev@student.vu.nl>
2025-07-10 13:58:23 +0200

    Implement custom layout pattern for AllCategoriesPage

--- src/pages/AllCategoriesRoutePage.vue
+++ src/pages/AllCategoriesRoutePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <AllCitiesPage
+  <AllCategoriesPage
   title="Все категории"
   :items="cities"
   :getItemLabel="(item) => item.name"
@@ -11,18 +11,22 @@
 </template>
 
 <script setup>
-import AllCitiesPage from './AllCitiesPage.vue'
+import AllCategoriesPage from './AllCategoriesPage.vue'
 import { ref } from 'vue'
 import { Icon } from '@iconify/vue'
 
 const cities = ref([
   { name: 'Экскурсии', attractions: 70, icon: 'ph:map-trifold' },
+  { name: 'Исторические места', attractions: 245, icon: 'mdi:castle' },
+  { name: 'Вечерний город', attractions: 123, icon: 'ph:moon-stars' },
   { name: 'Где поесть', attractions: 53, icon: 'ph:fork-knife' },
   { name: 'Размещение', attractions: 321, icon: 'ph:bed' },
-  { name: 'Исторические места', attractions: 245, icon: 'mdi:castle' },
   { name: 'Концерты', attractions: 76, icon: 'ph:music-notes' },
   { name: 'Идём семьёй', attractions: 72, icon: 'ph:users-three' },
   { name: 'Вечерний город', attractions: 123, icon: 'ph:moon-stars' },
+  { name: 'Вечерний город', attractions: 123, icon: 'ph:moon-stars' },
+  { name: 'Вечерний город', attractions: 123, icon: 'ph:moon-stars' },
+ 
 ])
 
 
